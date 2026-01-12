95369bfd3b5edd8dcf97a7114ebcf4b61f5b99b0
0x27sir42 <fizzanetworkai@gmail.com>
2026-01-13 02:59:03 +0700

    Update dashboardadmin.js

--- admin/dashboardadmin.js
+++ admin/dashboardadmin.js
@@ -1,32 +1,30 @@
 let provider, signer, admin;
 
 async function connectAdmin(){
- if(!window.ethereum) return alert("Wallet not found");
  await ethereum.request({method:"eth_requestAccounts"});
  provider=new ethers.providers.Web3Provider(window.ethereum);
  signer=provider.getSigner();
  admin=await signer.getAddress();
- document.getElementById("adminWallet").innerText="Admin: "+admin;
+ adminWallet.innerText=admin;
  loadSupport();
 }
 
 function loadSupport(){
- const list=document.getElementById("supportList");
- list.innerHTML="";
+ supportList.innerHTML="";
  for(let k in localStorage){
-   if(k.startsWith("support_")){
-     const user=k.replace("support_","");
-     const msg=localStorage.getItem(k);
-     list.innerHTML+=`
-       <div class="status">
-         <b>${user}</b><br>${msg}
-       </div>`;
-   }
+  if(k.startsWith("support_")){
+   const u=k.replace("support_","");
+   supportList.innerHTML+=`
+   <div class="status">
+    <b>${u}</b><br>
+    ${localStorage.getItem(k)}<br>
+    <input id="r_${u}" placeholder="Reply">
+    <button onclick="reply('${u}')">Send</button>
+   </div>`;
+  }
  }
 }
 
-async function withdraw(){
- const c=new ethers.Contract(PRESALE_ADDRESS,["function withdraw()"],signer);
- await (await c.withdraw()).wait();
- alert("Withdraw success");
+function reply(u){
+ localStorage.setItem("reply_"+u,document.getElementById("r_"+u).value);
 }
