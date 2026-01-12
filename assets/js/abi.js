a784cb307f90e7dc514e13552eed952fa8239e20
0x27sir42 <fizzanetworkai@gmail.com>
2026-01-13 02:27:29 +0700

    Update abi.js

--- assets/js/abi.js
+++ assets/js/abi.js
@@ -1,1 +1,13 @@
+const presaleABI=[
+ "function buy() payable",
+ "function claim()"
+];
 
+const tokenABI=[
+ "function approve(address,uint256)",
+ "function allowance(address,address) view returns(uint256)"
+];
+
+const stakingABI=[
+ "function stake(uint256,uint8)"
+];
