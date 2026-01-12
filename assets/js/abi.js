a83351cb2b57bfd5879b41a9c6876f2c1fb8fe1a
0x27sir42 <fizzanetworkai@gmail.com>
2026-01-13 03:19:33 +0700

    Update abi.js

--- assets/js/abi.js
+++ assets/js/abi.js
@@ -1,6 +1,3 @@
 const presaleABI=[
- "function buy() payable",
- "function claim()",
- "function totalRaised() view returns(uint256)",
- "function totalBuyers() view returns(uint256)"
+ "function buy() payable"
 ];
