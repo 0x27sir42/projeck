c6089a15448e3b2d3c8695082d2bf96c4cf8cef0
0x27sir42 <fizzanetworkai@gmail.com>
2026-01-13 02:29:29 +0700

    Update dashboardadmin.js

--- admin/dashboardadmin.js
+++ admin/dashboardadmin.js
@@ -1,1 +1,21 @@
+let provider,signer,admin;
 
+const PRESALE="0x72cF8781aa3A6D7FD3324CD0dAA8b858461849d7";
+
+const adminABI=[
+ "function withdraw()"
+];
+
+async function connectAdmin(){
+ await ethereum.request({method:"eth_requestAccounts"});
+ provider=new ethers.providers.Web3Provider(window.ethereum);
+ signer=provider.getSigner();
+ admin=await signer.getAddress();
+ document.getElementById("adminStatus").innerText="ADMIN "+admin;
+}
+
+async function withdraw(){
+ const c=new ethers.Contract(PRESALE,adminABI,signer);
+ await (await c.withdraw()).wait();
+ alert("WITHDRAW SUCCESS");
+}
